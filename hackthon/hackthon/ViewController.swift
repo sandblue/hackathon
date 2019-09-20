d5fd1328859a982f50dcd46417cc466aaed43242
Sarik Kumpan <kumpan.sarik@gmail.com>
2019-09-20 09:51:29 +0700

    add init file of proj

---
+++ hackthon/hackthon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hackthon
+//
+//  Created by Sarik Kumpan on 20/9/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hackthon/hackthonTests/hackthonTests.swift
@@ -0,0 +1,34 @@
+//
+//  hackthonTests.swift
+//  hackthonTests
+//
+//  Created by Sarik Kumpan on 20/9/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import XCTest
+@testable import hackthon
+
+class hackthonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
