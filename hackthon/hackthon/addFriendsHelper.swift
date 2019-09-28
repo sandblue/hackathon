d629e1f1fe076434789e341563a285a36a896df6
GDG <sonakul@o-kaatplus.com>
2019-09-28 22:58:49 +0700

    addFriendPhoto

---
+++ hackthon/hackthon/addFriendsHelper.swift
@@ -0,0 +1,79 @@
+//
+//  addFriendsHelper.swift
+//  hackthon
+//
+//  Created by GDG on 28/9/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import UIKit
+
+class addFriendsHelper: NSObject {
+    static let sharedInstance = addFriendsHelper()
+    
+    func imageOrientation(_ src:UIImage)->UIImage {
+        if src.imageOrientation == UIImage.Orientation.up {
+            return src
+        }
+        var transform: CGAffineTransform = CGAffineTransform.identity
+        switch src.imageOrientation {
+        case UIImage.Orientation.down, UIImage.Orientation.downMirrored:
+            transform = transform.translatedBy(x: src.size.width, y: src.size.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+            break
+        case UIImage.Orientation.left, UIImage.Orientation.leftMirrored:
+            transform = transform.translatedBy(x: src.size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(Double.pi/2))
+            break
+        case UIImage.Orientation.right, UIImage.Orientation.rightMirrored:
+            transform = transform.translatedBy(x: 0, y: src.size.height)
+            transform = transform.rotated(by: CGFloat(-Double.pi/2))
+            break
+        case UIImage.Orientation.up, UIImage.Orientation.upMirrored:
+            break
+        }
+        
+        switch src.imageOrientation {
+        case UIImage.Orientation.upMirrored, UIImage.Orientation.downMirrored:
+            transform.translatedBy(x: src.size.width, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+            break
+        case UIImage.Orientation.leftMirrored, UIImage.Orientation.rightMirrored:
+            transform.translatedBy(x: src.size.height, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+        case UIImage.Orientation.up, UIImage.Orientation.down, UIImage.Orientation.left, UIImage.Orientation.right:
+            break
+        }
+        
+        let ctx:CGContext = CGContext(data: nil, width: Int(src.size.width), height: Int(src.size.height), bitsPerComponent: (src.cgImage)!.bitsPerComponent, bytesPerRow: 0, space: (src.cgImage)!.colorSpace!, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!
+        
+        ctx.concatenate(transform)
+        
+        switch src.imageOrientation {
+        case UIImage.Orientation.left, UIImage.Orientation.leftMirrored, UIImage.Orientation.right, UIImage.Orientation.rightMirrored:
+            ctx.draw(src.cgImage!, in: CGRect(x: 0, y: 0, width: src.size.height, height: src.size.width))
+            break
+        default:
+            ctx.draw(src.cgImage!, in: CGRect(x: 0, y: 0, width: src.size.width, height: src.size.height))
+            break
+        }
+        
+        let cgimg:CGImage = ctx.makeImage()!
+        let img:UIImage = UIImage(cgImage: cgimg)
+        
+        return img
+    }
+}
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
---
+++ hackthon/hackthon/addFriendsViewController.swift
@@ -0,0 +1,102 @@
+//
+//  addFriendsViewController.swift
+//  hackthon
+//
+//  Created by GDG on 28/9/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import UIKit
+import ContactsUI
+class addFriendsViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
+
+    @IBOutlet weak var txfTel: UITextField!
+    @IBOutlet weak var imFace: UIImageView!
+    var chosenImage = UIImage()
+    let picker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
+        picker.delegate = self
+        let imgChangePhotoTap = UITapGestureRecognizer(target: self, action: #selector(self.imgChangePhotoTapped))
+        self.imFace.isUserInteractionEnabled = true
+        self.imFace.addGestureRecognizer(imgChangePhotoTap)
+        
+    }
+    
+    func showChooseImagePopup()
+    {
+        let actionsheet = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        actionsheet.addAction(UIAlertAction(title: "ถ่ายรูป", style: UIAlertAction.Style.default, handler: { (action) -> Void in
+            // show camera
+            self.cameraTapped();
+        }))
+        actionsheet.addAction(UIAlertAction(title: "อัลบั้ม", style: UIAlertAction.Style.default, handler: { (action) -> Void in
+            // image gallery
+            self.showImagePicker()
+        }))
+        actionsheet.addAction(UIAlertAction(title: "ยกเลิก", style: UIAlertAction.Style.cancel, handler: { (action) -> Void in
+            // Cancel
+        }))
+        self.present(actionsheet, animated: true, completion: nil)
+    }
+    
+    // Show Picker
+    func showImagePicker()
+    {
+        self.picker.allowsEditing = true
+        self.picker.sourceType = .photoLibrary
+        self.picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        self.picker.modalPresentationStyle = .fullScreen
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func cameraTapped()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            self.picker.allowsEditing = true
+            self.picker.sourceType = UIImagePickerController.SourceType.camera
+            self.picker.cameraCaptureMode = .photo
+            self.picker.modalPresentationStyle = .fullScreen
+            present(self.picker, animated: true, completion: nil)
+        } else {
+            noCamera()
+        }
+    }
+    
+    func noCamera(){
+        let alertVC = UIAlertController(
+            title: "No Camera",
+            message: "Sorry, this device has no camera",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage]  as? UIImage else {
+            return
+        }
+        self.chosenImage = addFriendsHelper.sharedInstance.imageOrientation(image)
+        self.imFace.image = self.chosenImage
+        dismiss(animated: true) {
+           
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imgChangePhotoTapped(sender:UITapGestureRecognizer) {
+        self.showChooseImagePopup()
+    }
+    
+}
