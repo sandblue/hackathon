85c909fafd52d9dcbc8b173c780a020cecc99cee
Jiratip <a5830017a@gmail.com>
2019-10-01 22:25:37 +0700

    add camera

---
+++ hackthon/hackthon/CameraSecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CameraSecondViewController.swift
+//  hackthon
+//
+//  Created by Jiratip Hemwutthipan on 1/10/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import UIKit
+
+class CameraSecondViewController: ViewController {
+    
+    @IBOutlet weak var testLbl : UILabel!
+    
+    var dataLastFromPage = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        testLbl.text = dataLastFromPage
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
