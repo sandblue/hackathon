47a3d7f2f2351ae6fd249b9465dbe1149812065c
Jiratip <a5830017a@gmail.com>
2019-10-02 19:29:56 +0700

    perform segue

---
+++ hackthon/hackthon/friendListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  friendListViewController.swift
+//  hackthon
+//
+//  Created by Jiratip Hemwutthipan on 2/10/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import UIKit
+
+class friendListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
